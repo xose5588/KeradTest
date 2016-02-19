5ebbe11cbc8c08788b25e53f9cecb4e55c209225
JManrique <jose.manrique.carrillo@gmail.com>
2016-02-19 13:15:52 +0100

    [FIX] Change log for Toast

--- app/src/main/java/jmanrique/com/keradtest/views/MainActivity.java
+++ app/src/main/java/jmanrique/com/keradtest/views/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -76,7 +75,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void failure(TwitterException exception) {
-                Log.d("TwitterKit", "Login with Twitter failure", exception);
+                Toast.makeText(getApplicationContext(), "Login with Twitter failure", Toast.LENGTH_LONG).show();
             }
         });
     }
